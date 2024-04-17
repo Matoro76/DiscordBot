bea2b76ad30faacda76c14e10d3d098bee8baae2
Matoro76 <qa880706@gmail.com>
2024-04-17 17:42:29 +0800

    補上guildId

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 let config;
 
 if (process.env.NODE_ENV === 'production') {
-    config = {
-        token: process.env.TOKEN,
-    };
+  config = {
+    token: process.env.TOKEN,
+    guildId: process.env.GUILD_ID,
+  };
 } else {
-    config = require('./config.json');
+  config = require('./config.json');
 }
 
 module.exports = config;
